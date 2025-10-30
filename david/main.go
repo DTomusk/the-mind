74acb7b74d6cc345168ceea48cc2c6f6d5a9272f
Dave <david.tomusk@gmail.com>
2025-10-30 11:48:02 +0000

    Add createDeck func

--- david/main.go
+++ david/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, David!")
+	deck := createDeck()
+	fmt.Println("Deck of cards:", deck)
+}
+
+// initialize a new deck of cards numbered 1 to 100
+func createDeck() []int {
+	fmt.Println("Creating a new deck of cards...")
+	deck := make([]int, 100)
+	for i := 0; i < 100; i++ {
+		deck[i] = i + 1
+	}
+	return deck
 }
